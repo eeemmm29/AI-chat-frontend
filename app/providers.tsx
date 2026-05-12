e8b43eeab9af836d3d13c70a8d250aec85729966
Emir <148708322+eeemmm29@users.noreply.github.com>
2026-05-12 13:39:22 +0600

    feat: wrap application with AuthProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,11 +5,17 @@ import type { ThemeProviderProps } from "next-themes";
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+import { AuthProvider } from "@/context/auth-context";
+
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-  return <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>;
+  return (
+    <AuthProvider>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </AuthProvider>
+  );
 }
